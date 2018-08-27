608e22958288e365f6879b9e2d8ba6aa97f85ba8
Soma Shekar Oruganti <soruganti@salesforce.com>
2018-08-27 01:33:31 -0700

    Updating the script to create and check status of Jenkins instance

--- src/main/resources/scripts/statusScript.sh
+++ src/main/resources/scripts/statusScript.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
-#only echo should be Jenkins URL
-echo "script to know status is running"
+KUBE_USERNAME=''
+KUBE_PASSWD=''
 
-exit 0
+HELM_IMAGE_TAG='913a96c'
 
+docker run --rm -e KUBE_USERNAME=${KUBE_USERNAME} -e KUBE_PASSWD=${KUBE_PASSWD} -e CI_NAME=${CI_NAME} somashekar10/helm:${HELM_IMAGE_TAG} /root/check_ci.sh
